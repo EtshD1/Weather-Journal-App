af9c09fba61f471cab85d2428f9b972b437f682c
EtshD1 <H79Pain@Gmail.com>
2020-12-14 16:18:49 +0200

    add middleware and serve website files

--- server.js
+++ server.js
@@ -3,3 +3,11 @@ const express = require("express"),
   app = express(),
   cors = require("cors"),
   bodyParser = require("body-parser");
+// Variables
+const projectData = {};
+// Middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
+// Initialize website folder
+app.use(express.static('website'));
