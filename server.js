ba6c89d8a10f37fb5940cdeacf19ab42f45bdd76
EtshD1 <H79Pain@Gmail.com>
2020-12-14 21:43:11 +0200

    add homepage route and start server

--- server.js
+++ server.js
@@ -1,13 +1,26 @@
-// Dependencies
+/* Dependencies */
 const express = require("express"),
   app = express(),
   cors = require("cors"),
-  bodyParser = require("body-parser");
-// Variables
+  bodyParser = require("body-parser"),
+  port = 8080;
+/* Variables */
 const projectData = {};
-// Middleware
+/* Middlewares*/
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+// Cors for cross origin allowance
 app.use(cors());
 // Initialize website folder
 app.use(express.static('website'));
+/*  Callback functions */
+const callbacks = {
+  homepage: (req, res) => { res.sendFile("./index.html"); }
+};
+/* Routes */
+// GET: homepage
+app.get("/", callbacks.homepage);
+/* Start up Server */
+app.listen(port, () => {
+  console.log(`Server running at ${port}`);
+});
